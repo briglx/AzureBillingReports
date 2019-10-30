4600399b3cec3aeb1bdc138bdc85409f18419adf
Brig Lamoreaux <bklamoreaux@gmail.com>
2019-10-29 20:41:54 -0700

    Add ri type.

--- script/get_ri_recommendations.py
+++ script/get_ri_recommendations.py
@@ -8,36 +8,37 @@ import csv
 
 import requests
 
+RI_TYPES = ["Shared", "Single"]
 HOST_NAME = "https://consumption.azure.com/v2/enrollments/%s"
-API_PATH = "/SharedReservationRecommendations?lookBackPeriod=%s"
+API_PATH = "/%sReservationRecommendations?lookBackPeriod=%s"
 
 
-def get_uri(eid, look_back_period):
+def get_uri(eid, ri_type, look_back_period):
     r"""Return a consumption uri."""
     path_url = HOST_NAME + API_PATH
-    uri = path_url % (eid, look_back_period)
+    uri = path_url % (eid, ri_type, look_back_period)
     return uri
 
 
-def get_last_week_uri(eid):
+def get_last_week_uri(eid, ri_type):
     r"""Return a consumption uri for last week."""
     look_back_period = "7"
-    return get_uri(eid, look_back_period)
+    return get_uri(eid, ri_type, look_back_period)
 
 
-def get_last_30_days_uri(eid):
+def get_last_30_days_uri(eid, ri_type):
     r"""Return a consumption uri for last 30 days."""
     look_back_period = "30"
-    return get_uri(eid, look_back_period)
+    return get_uri(eid, ri_type, look_back_period)
 
 
-def get_most_data_uri(eid):
+def get_most_data_uri(eid, ri_type):
     r"""Return a consumption uri for last 60 days."""
     look_back_period = "60"
-    return get_uri(eid, look_back_period)
+    return get_uri(eid, ri_type, look_back_period)
 
 
-def get_recommendations(uri, auth_key):
+def get_recommendations(uri, auth_key, ri_type):
     r"""Download recommendations.."""
     print("Calling uri " + uri)
 
@@ -53,7 +54,8 @@ def get_recommendations(uri, auth_key):
 
     header = list(recommendations[0].keys())[1:-1]
 
-    with open("ri_recommendations.csv", "w", newline="") as csvfile:
+    file_name = "ri_" + ri_type.lower() + "_recommendations.csv"
+    with open(file_name, "w", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
 
         writer.writerows([header])
@@ -69,8 +71,9 @@ def main(argv):
     eid = argv[0]
     auth_key = argv[1]
 
-    uri = get_most_data_uri(eid)
-    get_recommendations(uri, auth_key)
+    for ri_type in RI_TYPES:
+        uri = get_most_data_uri(eid, ri_type)
+        get_recommendations(uri, auth_key, ri_type)
 
 
 if __name__ == "__main__":
