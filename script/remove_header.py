baf6f26d8a91817050dbfd7d75bb24277c6370a1
Brig Lamoreaux <bklamoreaux@gmail.com>
2019-11-05 14:20:30 -0700

    Rename temp.

--- script/remove_header.py
+++ script/remove_header.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python
 """Script to remove metadata header information from  monthly billing data."""
+import os
 import sys
 import shutil
 
 
 def remove_first_lines(file_name, count):
     """Remove first count of lines from file_name."""
-    with open(file_name, "r") as source_file, \
-        open(file_name + ".tmp", "w") as target_file:
+    with open(file_name, "r") as source_file, open(
+        file_name + ".tmp", "w"   # pylint: disable=C0330
+    ) as target_file:
 
         for _ in range(count):
             source_file.readline()
 
         shutil.copyfileobj(source_file, target_file)
-        shutil.move(target_file, source_file)
+
+        os.remove(source_file)
+        os.rename(target_file, source_file)
 
 
 def main(argv):
